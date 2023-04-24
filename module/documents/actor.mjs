18c2bb8ec3dbde5219f694360f9ffdda92c98594
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-04-23 22:57:51 -0600

    removed/commented out unused js

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -6,18 +6,11 @@ export class StoneburnerActor extends Actor {
 
   /** @override */
   prepareData() {
-    // Prepare data for the actor. Calling the super version of this executes
-    // the following, in order: data reset (to clear active effects),
-    // prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
-    // prepareDerivedData().
     super.prepareData();
   }
 
   /** @override */
-  prepareBaseData() {
-    // Data modifications in this step occur before processing embedded
-    // documents or derived data.
-  }
+  // prepareBaseData() {}
 
   /**
    * @override
@@ -31,7 +24,6 @@ export class StoneburnerActor extends Actor {
   prepareDerivedData() {
     const actorData = this;
     const systemData = actorData.system;
-    const flags = actorData.flags.stoneburner || {};
 
     // Make separate methods for each Actor type (character, npc, etc.) to keep
     // things organized.
@@ -47,12 +39,6 @@ export class StoneburnerActor extends Actor {
 
     // Make modifications to data here. For example:
     const systemData = actorData.system;
-
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(systemData.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
   }
 
   /**
@@ -63,7 +49,6 @@ export class StoneburnerActor extends Actor {
 
     // Make modifications to data here. For example:
     const systemData = actorData.system;
-    systemData.xp = (systemData.cr * systemData.cr) * 100;
   }
 
   /**
@@ -84,19 +69,6 @@ export class StoneburnerActor extends Actor {
    */
   _getCharacterRollData(data) {
     if (this.type !== 'character') return;
-
-    // Copy the ability scores to the top level, so that rolls can use
-    // formulas like `@str.mod + 4`.
-    if (data.abilities) {
-      for (let [k, v] of Object.entries(data.abilities)) {
-        data[k] = foundry.utils.deepClone(v);
-      }
-    }
-
-    // Add level for easier access, or fall back to 0.
-    if (data.attributes.level) {
-      data.lvl = data.attributes.level.value ?? 0;
-    }
   }
 
   /**
@@ -104,8 +76,6 @@ export class StoneburnerActor extends Actor {
    */
   _getNpcRollData(data) {
     if (this.type !== 'npc') return;
-
-    // Process additional NPC data here.
   }
 
 }
--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1,5 +1,3 @@
-import {onManageActiveEffect, prepareActiveEffectCategories} from "../helpers/effects.mjs";
-
 /**
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
@@ -12,8 +10,8 @@ export class StoneburnerActorSheet extends ActorSheet {
       classes: ["stoneburner", "sheet", "actor"],
       template: "systems/stoneburner/templates/actor/actor-sheet.html",
       width: 600,
-      height: 600,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "features" }]
+      height: 700,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "skills-items" }]
     });
   }
 
@@ -26,10 +24,6 @@ export class StoneburnerActorSheet extends ActorSheet {
 
   /** @override */
   getData() {
-    // Retrieve the data structure from the base sheet. You can inspect or log
-    // the context variable to see the structure, but some key properties for
-    // sheets are the actor object, the data object, whether or not it's
-    // editable, the items array, and the effects array.
     const context = super.getData();
 
     // Use a safe clone of the actor data for further operations.
@@ -37,7 +31,6 @@ export class StoneburnerActorSheet extends ActorSheet {
 
     // Add the actor's data to context.data for easier access, as well as flags.
     context.system = actorData.system;
-    context.flags = actorData.flags;
 
     // Prepare character data and items.
     if (actorData.type == 'character') {
@@ -53,9 +46,6 @@ export class StoneburnerActorSheet extends ActorSheet {
     // Add roll data for TinyMCE editors.
     context.rollData = context.actor.getRollData();
 
-    // Prepare active effects
-    context.effects = prepareActiveEffectCategories(this.actor.effects);
-
     return context;
   }
 
@@ -68,9 +58,6 @@ export class StoneburnerActorSheet extends ActorSheet {
    */
   _prepareCharacterData(context) {
     // Handle ability scores.
-    for (let [k, v] of Object.entries(context.system.abilities)) {
-      // v.label = game.i18n.localize(CONFIG.STONEBURNER.abilities[k]) ?? k;
-    }
   }
 
   /**
@@ -82,44 +69,44 @@ export class StoneburnerActorSheet extends ActorSheet {
    */
   _prepareItems(context) {
     // Initialize containers.
-    const gear = [];
-    const features = [];
-    const spells = {
-      0: [],
-      1: [],
-      2: [],
-      3: [],
-      4: [],
-      5: [],
-      6: [],
-      7: [],
-      8: [],
-      9: []
-    };
+    // const gear = [];
+    // const features = [];
+    // const spells = {
+    //   0: [],
+    //   1: [],
+    //   2: [],
+    //   3: [],
+    //   4: [],
+    //   5: [],
+    //   6: [],
+    //   7: [],
+    //   8: [],
+    //   9: []
+    // };
 
-    // Iterate through items, allocating to containers
-    for (let i of context.items) {
-      i.img = i.img || DEFAULT_TOKEN;
-      // Append to gear.
-      if (i.type === 'item') {
-        gear.push(i);
-      }
-      // Append to features.
-      else if (i.type === 'feature') {
-        features.push(i);
-      }
-      // Append to spells.
-      else if (i.type === 'spell') {
-        if (i.system.spellLevel != undefined) {
-          spells[i.system.spellLevel].push(i);
-        }
-      }
-    }
+    // // Iterate through items, allocating to containers
+    // for (let i of context.items) {
+    //   i.img = i.img || DEFAULT_TOKEN;
+    //   // Append to gear.
+    //   if (i.type === 'item') {
+    //     gear.push(i);
+    //   }
+    //   // Append to features.
+    //   else if (i.type === 'feature') {
+    //     features.push(i);
+    //   }
+    //   // Append to spells.
+    //   else if (i.type === 'spell') {
+    //     if (i.system.spellLevel != undefined) {
+    //       spells[i.system.spellLevel].push(i);
+    //     }
+    //   }
+    // }
 
     // Assign and return
-    context.gear = gear;
-    context.features = features;
-    context.spells = spells;
+    // context.gear = gear;
+    // context.features = features;
+    // context.spells = spells;
   }
 
   /* -------------------------------------------- */
@@ -129,29 +116,26 @@ export class StoneburnerActorSheet extends ActorSheet {
     super.activateListeners(html);
 
     // Render the item sheet for viewing/editing prior to the editable check.
-    html.find('.item-edit').click(ev => {
-      const li = $(ev.currentTarget).parents(".item");
-      const item = this.actor.items.get(li.data("itemId"));
-      item.sheet.render(true);
-    });
+    // html.find('.item-edit').click(ev => {
+    //   const li = $(ev.currentTarget).parents(".item");
+    //   const item = this.actor.items.get(li.data("itemId"));
+    //   item.sheet.render(true);
+    // });
 
     // -------------------------------------------------------------
     // Everything below here is only needed if the sheet is editable
     if (!this.isEditable) return;
 
     // Add Inventory Item
-    html.find('.item-create').click(this._onItemCreate.bind(this));
+    // html.find('.item-create').click(this._onItemCreate.bind(this));
 
     // Delete Inventory Item
-    html.find('.item-delete').click(ev => {
-      const li = $(ev.currentTarget).parents(".item");
-      const item = this.actor.items.get(li.data("itemId"));
-      item.delete();
-      li.slideUp(200, () => this.render(false));
-    });
-
-    // Active Effect management
-    html.find(".effect-control").click(ev => onManageActiveEffect(ev, this.actor));
+    // html.find('.item-delete').click(ev => {
+    //   const li = $(ev.currentTarget).parents(".item");
+    //   const item = this.actor.items.get(li.data("itemId"));
+    //   item.delete();
+    //   li.slideUp(200, () => this.render(false));
+    // });
 
     // Rollable abilities.
     html.find('.rollable').click(this._onRoll.bind(this));
@@ -172,27 +156,27 @@ export class StoneburnerActorSheet extends ActorSheet {
    * @param {Event} event   The originating click event
    * @private
    */
-  async _onItemCreate(event) {
-    event.preventDefault();
-    const header = event.currentTarget;
-    // Get the type of item to create.
-    const type = header.dataset.type;
-    // Grab any data associated with this control.
-    const data = duplicate(header.dataset);
-    // Initialize a default name.
-    const name = `New ${type.capitalize()}`;
-    // Prepare the item object.
-    const itemData = {
-      name: name,
-      type: type,
-      system: data
-    };
-    // Remove the type from the dataset since it's in the itemData.type prop.
-    delete itemData.system["type"];
+  // async _onItemCreate(event) {
+  //   event.preventDefault();
+  //   const header = event.currentTarget;
+  //   // Get the type of item to create.
+  //   const type = header.dataset.type;
+  //   // Grab any data associated with this control.
+  //   const data = duplicate(header.dataset);
+  //   // Initialize a default name.
+  //   const name = `New ${type.capitalize()}`;
+  //   // Prepare the item object.
+  //   const itemData = {
+  //     name: name,
+  //     type: type,
+  //     system: data
+  //   };
+  //   // Remove the type from the dataset since it's in the itemData.type prop.
+  //   delete itemData.system["type"];
 
-    // Finally, create the item!
-    return await Item.create(itemData, {parent: this.actor});
-  }
+  //   // Finally, create the item!
+  //   return await Item.create(itemData, {parent: this.actor});
+  // }
 
   /**
    * Handle clickable rolls.
