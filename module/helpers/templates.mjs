d9cf155a2befda8ebce25e7faa8f500195802a57
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-04-23 19:50:11 -0600

    update 2

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -9,7 +9,9 @@
     // Actor partials.
     "systems/stoneburner/templates/actor/parts/actor-skills.html",
     "systems/stoneburner/templates/actor/parts/actor-items.html",
-    "systems/stoneburner/templates/actor/parts/actor-spells.html",
-    "systems/stoneburner/templates/actor/parts/actor-effects.html",
+    "systems/stoneburner/templates/actor/parts/actor-abilities.html",
+    "systems/stoneburner/templates/actor/parts/actor-artifacts.html",
+    "systems/stoneburner/templates/actor/parts/actor-projects.html",
+    "systems/stoneburner/templates/actor/parts/actor-details.html"
   ]);
 };
